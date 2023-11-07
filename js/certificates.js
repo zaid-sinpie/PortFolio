58a7b47f06e63c91877ebd943553f4e3b97ca82d
zaid-sinpie <zaidmasuldar@gmail.com>
2023-11-07 22:02:44 +0530

    added new certificates

--- js/certificates.js
+++ js/certificates.js
@@ -5,6 +5,8 @@
 
 const ibmCer = document.querySelector('.ibm');
 const infosysCer = document.querySelector('.infosys');
+const infosysIotCer = document.querySelector('.infosysIot');
+const infosysHighCer = document.querySelector('.infosysHigh');
 const pantechCer = document.querySelector('.pantech');
 const iotBootcampCer = document.querySelector('.iotBootcamp');
 const cyberCer = document.querySelector('.cyber');
@@ -17,6 +19,7 @@ ibmCer.addEventListener('click', function(){
     image.src = '../certificates/IBM-0.jpg';
     ibmCer.style.backgroundColor = '#fe2e52';
     infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#8e1d42'
     pantechCer.style.backgroundColor = '#8e1d42';
     iotBootcampCer.style.backgroundColor = '#8e1d42';
     cyberCer.style.backgroundColor = '#8e1d42';
@@ -26,6 +29,31 @@ infosysCer.addEventListener('click',function(){
     image.src = '../certificates/Info-1.jpg';
     ibmCer.style.backgroundColor = '#8e1d42';
     infosysCer.style.backgroundColor = '#fe2e52';
+    infosysIotCer.style.backgroundColor = '#8e1d42'
+    infosysHighCer.style.backgroundColor = '#8e1d42'
+    pantechCer.style.backgroundColor = '#8e1d42';
+    iotBootcampCer.style.backgroundColor = '#8e1d42';
+    cyberCer.style.backgroundColor = '#8e1d42';
+});
+
+infosysIotCer.addEventListener('click',function(){
+    image.src = '../certificates/InfoIot.jpg';
+    ibmCer.style.backgroundColor = '#8e1d42';
+    infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#fe2e52'
+    infosysHighCer.style.backgroundColor = '#8e1d42'
+    infosysHighCer.style.backgroundColor = '#8e1d42';
+    pantechCer.style.backgroundColor = '#8e1d42';
+    iotBootcampCer.style.backgroundColor = '#8e1d42';
+    cyberCer.style.backgroundColor = '#8e1d42';
+});
+
+infosysHighCer.addEventListener('click', function(){
+    image.src = '../certificates/infoHighImpactPresentation.jpg';
+    ibmCer.style.backgroundColor = '#8e1d42';
+    infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#8e1d42';
+    infosysHighCer.style.backgroundColor = '#fe2e52';
     pantechCer.style.backgroundColor = '#8e1d42';
     iotBootcampCer.style.backgroundColor = '#8e1d42';
     cyberCer.style.backgroundColor = '#8e1d42';
@@ -35,6 +63,8 @@ pantechCer.addEventListener('click', function(){
     image.src = '../certificates/pantech-3.jpg';
     ibmCer.style.backgroundColor = '#8e1d42';
     infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#8e1d42';
+    infosysHighCer.style.backgroundColor = '#8e1d42';
     pantechCer.style.backgroundColor = '#fe2e52';
     iotBootcampCer.style.backgroundColor = '#8e1d42';
     cyberCer.style.backgroundColor = '#8e1d42';
@@ -44,6 +74,8 @@ iotBootcampCer.addEventListener('click', function(){
     image.src = '';
     ibmCer.style.backgroundColor = '#8e1d42';
     infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#8e1d42';
+    infosysHighCer.style.backgroundColor = '#8e1d42';
     pantechCer.style.backgroundColor = '#8e1d42';
     iotBootcampCer.style.backgroundColor = '#fe2e52';
     cyberCer.style.backgroundColor = '#8e1d42';
@@ -53,6 +85,8 @@ cyberCer.addEventListener('click', function(){
     image.src = '';
     ibmCer.style.backgroundColor = '#8e1d42';
     infosysCer.style.backgroundColor = '#8e1d42';
+    infosysIotCer.style.backgroundColor = '#8e1d42';
+    infosysHighCer.style.backgroundColor = '#8e1d42';
     pantechCer.style.backgroundColor = '#8e1d42';
     iotBootcampCer.style.backgroundColor = '#8e1d42';
     cyberCer.style.backgroundColor = '#fe2e52';
