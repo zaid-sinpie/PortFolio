8a6b5ba0293e469bead5489b971b871c1f411e22
zaid-sinpie <zaidmasuldar@gmail.com>
2023-11-18 19:10:44 +0530

    contact responsive

--- js/contact.js
+++ js/contact.js
@@ -4,10 +4,10 @@ const mailForm = document.getElementById('mail-form');
 
 mailForm.addEventListener('submit', function(event){
     event.preventDefault();
-    name = mailForm.elements['name'];
-    email = mailForm.elements['email'];
-    subject = mailForm.elements['subject'];
-    message = mailForm.elements['message'];
+    const name = mailForm.elements['name'];
+    const email = mailForm.elements['email'];
+    const subject = mailForm.elements['subject'];
+    const message = mailForm.elements['message'];
     fetch('https://portfolio-mail-server.onrender.com/sendmail',{
         method:'POST',body: JSON.stringify({name,email,subject,message})
     });
